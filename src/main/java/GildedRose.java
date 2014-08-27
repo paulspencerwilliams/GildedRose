a610b4458b47e6ee1b2a6d2dba61ed4fbd3934ee
Paul Williams <paul@paulswilliams.me.uk>
2014-08-27 16:49:25 +0100

    Added tests

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -4,33 +4,36 @@ import java.util.List;
 
 public class GildedRose {
 
-	private static List<Item> items = null;
+	private List<Item> items = null;
 
-	/**
-	 * @param args
-	 */
+    public GildedRose(ArrayList<Item> items) {
+        this.items = items;
+    }
+
 	public static void main(String[] args) {
 		
         System.out.println("OMGHAI!");
-		
-        items = new ArrayList<Item>();
-        items.add(new Item("+5 Dexterity Vest", 10, 20));
-        items.add(new Item("Aged Brie", 2, 0));
-        items.add(new Item("Elixir of the Mongoose", 5, 7));
-        items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-        items.add(new Item("Conjured Mana Cake", 3, 6));
 
-        updateQuality();
+
+        ArrayList<Item> defaultItems= new ArrayList<Item>();
+        defaultItems.add(new Item("+5 Dexterity Vest", 10, 20));
+        defaultItems.add(new Item("Aged Brie", 2, 0));
+        defaultItems.add(new Item("Elixir of the Mongoose", 5, 7));
+        defaultItems.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+        defaultItems.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+        defaultItems.add(new Item("Conjured Mana Cake", 3, 6));
+        GildedRose gildedRose = new GildedRose(defaultItems);
+
+        gildedRose.updateQuality();
 }
 
 
 	
-    public static void updateQuality()
+    public void updateQuality()
     {
         for (int i = 0; i < items.size(); i++)
         {
-            if ((!"Aged Brie".equals(items.get(i).getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName())) 
+            if ((!"Aged Brie".equals(items.get(i).getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
             {
                 if (items.get(i).getQuality() > 0)
                 {
--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -1,12 +1,90 @@
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 
 public class GildedRoseTest {
 
-	@Test
-	public void testTheTruth() {
-		assertTrue(true);
-	}
+    @Test
+    public void qualityReducesByOne ()
+    {
+        assertThat(updateQuality("anItem", 2, 1).getQuality(), equalTo(0));
+    }
+
+    @Test
+    public void sellInIsDecrementedByOne ()
+    {
+        assertThat(updateQuality("anItem", 2, 1).getSellIn(), equalTo(1));
+    }
+
+    @Test
+    public void qualityReducesByTwoOnceExpired ()
+    {
+        assertThat(updateQuality("anItem", 0, 4).getQuality(), equalTo(2));
+    }
+
+    @Test
+    public void brieIncreasesInQualityBy1BeforeItExpires ()
+    {
+        assertThat(updateQuality("Aged Brie", 5, 5).getQuality(), equalTo(6));
+    }
+
+    @Test
+    public void brieIncreasesInQualityBy2AfterItExpires ()
+    {
+        assertThat(updateQuality("Aged Brie", 0, 5).getQuality(), equalTo(7));
+    }
+
+    @Test
+    public void qualityIsLimitedTo50 ()
+    {
+        assertThat(updateQuality("Aged Brie", 0, 50).getQuality(), equalTo(50));
+    }
+
+    @Test
+    public void SulfurasDontReduce()
+    {
+        assertThat(updateQuality("Sulfuras, Hand of Ragnaros", 0, 5).getQuality(), equalTo(5));
+    }
+
+    @Test
+    public void SulfurasDontHaveSellIn()
+    {
+        assertThat(updateQuality("Sulfuras, Hand of Ragnaros", 5, 5).getQuality(), equalTo(5));
+    }
+
+    @Test
+    public void BackstagePassesIncreaseBy1Until10DaysToGo ()
+    {
+        assertThat(updateQuality("Backstage passes to a TAFKAL80ETC concert", 12, 5).getQuality(), equalTo(6));
+    }
+
+    @Test
+    public void BackstagePassesIncreaseBy2Until10DaysToGo ()
+    {
+        assertThat(updateQuality("Backstage passes to a TAFKAL80ETC concert", 10, 5).getQuality(), equalTo(7));
+    }
+
+    @Test
+    public void BackstagePassesIncreaseBy3Until5DaysToGo ()
+    {
+        assertThat(updateQuality("Backstage passes to a TAFKAL80ETC concert", 5, 5).getQuality(), equalTo(8));
+    }
+
+    @Test
+    public void BackstagePassesIncreaseUntilSellIn ()
+    {
+        assertThat(updateQuality("Backstage passes to a TAFKAL80ETC concert", 0, 5).getQuality(), equalTo(0));
+    }
+
+    private Item updateQuality(String name, int sellIn, int quality) {
+        ArrayList<Item> items = new ArrayList<Item>();
+        Item simpleItem = new Item(name, sellIn, quality);
+        items.add(simpleItem);
+        new GildedRose(items).updateQuality();
+        return simpleItem;
+    }
 }
